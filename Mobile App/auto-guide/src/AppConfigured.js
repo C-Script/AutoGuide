dac240f0c108946bbc8910fc201c2d790c54dc67
Abdo <abdopoetry@gmail.com>
2019-04-20 19:52:19 +0200

    Add app skeleton again with eslint configurations

---
+++ Mobile App/auto-guide/src/AppConfigured.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const AppConfigured = () => (
+  <View>
+    <Text>AppConfigured</Text>
+  </View>
+);
+
+AppConfigured.propTypes = {};
+
+export default AppConfigured;
