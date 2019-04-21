416c5ef5efa3cb16578e10f7ecbe11fef06bc827
Mahmoud-Eltobgy <15X0069@eng.asu.edu.eg>
2019-04-21 17:02:53 +0200

    looping through all images ,and saving features to file the ExtractedF.csv

--- Scripts/AutoGuide.py
+++ Scripts/AutoGuide.py
@@ -1,10 +1,59 @@
-import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+import cv2 
+import csv
+import glob
+im=cv2.imread('ak1.jpg')
+# print(im)
+# cv.xfeatures2d.SIFT_create()
+foldersPaths=[r'D:\Auto Guide\AutoGuide\Data\train\Characters\Akhenaten',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\Cleopatra vii',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\hatshepsut',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\khufu',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\Nefrtiti',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\rahotep',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\rahotep and his wife',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\sesostris i',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\sesostris iii',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\sheik el balad',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\the dwarf seneb and his family',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\The seated scribe',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\Thutmose III',
+r'D:\Auto Guide\AutoGuide\Data\train\Characters\Tutankhamoun',
+r'D:\Auto Guide\AutoGuide\Data\train\Objects\apis',
+r'D:\Auto Guide\AutoGuide\Data\train\Objects\ceremonial chair of tutankhamoun',
+r'D:\Auto Guide\AutoGuide\Data\train\Objects\The bed of tutankhamoun',
+r'D:\Auto Guide\AutoGuide\Data\train\Objects\The chair of tut',
+r'D:\Auto Guide\AutoGuide\Data\train\Objects\The dagger of tut'
+]
+filename='extractedF.csv'
+
+def writeTofile(row,fileName):
+    # table=dgv.get_items()[3:]
+    with open(fileName, mode='w', newline='') as f:
+        file_writer = csv.writer(f,delimiter=',',dialect="excel", lineterminator="\n")
+        file_writer.writerow(row)
+def appendToFile(row,fileName):
+    # table=dgv.get_items()[3:]
+    with open(fileName, mode='a', newline='') as f:
+        file_writer = csv.writer(f,delimiter=',',dialect="excel", lineterminator="\n")
+        # print(row)
+        file_writer.writerow(row)
+def getFromFile(fileName):
+    contents=[]
+    with open(fileName, mode='r', newline='') as f:
+        file_reader = csv.reader(f,delimiter=';',dialect="excel", lineterminator="\n")
+        for row in file_reader:
+            contents.append(row)
+        # print(contents)
+        # sys.exit()
+        return (contents)
+
+
 
 def exists(image, template,method,thresh):
 
-    digit_res = cv.matchTemplate(image, template,method)
+    digit_res = cv2.matchTemplate(image, template,method)
     loc = np.where(digit_res >= thresh)
 
     if len(loc[-1]) == 0:
@@ -16,36 +65,41 @@ def exists(image, template,method,thresh):
 
     return True ,digit_res
 
+def gen_sift_features(gray_img):
+    sift = cv2.xfeatures2d.SIFT_create()
+    # kp is the keypoints
+    #
+    # desc is the SIFT descriptors, they're 128-dimensional vectors
+    # that we can use for our final features
+    kp, desc = sift.detectAndCompute(gray_img, None)
+    return kp, desc
+def show_sift_features(gray_img, color_img, kp):
+    return plt.imshow(cv2.drawKeypoints(gray_img, kp, color_img.copy()))
 
-img = cv.imread('nf_image1.jpg',0)
-img2 = img.copy()
-template = cv.imread('nf_template_e.jpg',0)
-w, h = template.shape[::-1]
-threshold=0.95
-title=''
-# All the 6 methods for comparison in a list
-methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-            'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
-for meth in methods:
-    img = img2.copy()
-    method = eval(meth)
-    # Apply template Matching
-    flag,res = exists(img,template,method,threshold)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(img,top_left, bottom_right, 255, 2)
-    if not flag :
-        title='Wrong Detection'
-    else:
-        title='Detected point'
-    plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    plt.title(title), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
-    plt.show()
+def to_gray(color_img):
+    gray = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
+    return gray
+def getFolderImages(folderPath,ext='jpg'):
+    images = [cv2.imread(f) for f in glob.glob(folderPath+'\*.'+ext)]
+    return images
+def saveExtractedFeatures():
+        for i,folder in enumerate(foldersPaths):
+                images=getFolderImages(folder)
+                for image in images:
+                        grayimage=to_gray(image)
+                        kp,desc=gen_sift_features(grayimage)
+                        kp.append(i)
+                        if(i==0):
+                                writeTofile(kp,filename)
+                        else:
+                                appendToFile(kp,filename)
+
+
+saveExtractedFeatures()
+# grayimg=to_gray(im)
+# kp1,desc2=gen_sift_features(grayimg)
+# print(desc2)
+# show_sift_features(grayimg,im,kp1)
+# plt.show()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
